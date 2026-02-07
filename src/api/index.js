ed499b2aa00688d2f21305bdaf73c5841ddf9839
parmodpatel <parmodpatel2603@gmail.com>
2026-02-07 15:33:30 +0530

    client URL fix for backend

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = process.env.REACT_APP_API_URL || "http://localhost:8000";
+const API_BASE_URL = process.env.REACT_APP_API_URL || "https://memories-server-z1p4.onrender.com";
 
 const API = axios.create({
   baseURL: API_BASE_URL,
